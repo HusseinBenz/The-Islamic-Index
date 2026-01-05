11ce62ee07c43cc8d993f2a73c201d8ebbe2ccff
Hamza <252877785+HusseinBenz@users.noreply.github.com>
2026-01-05 22:24:22 +0100

    set config.ts

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,61 @@
+import { createRequire } from 'module'
+import { defineAdditionalConfig, type DefaultTheme } from 'vitepress'
+
+const require = createRequire(import.meta.url)
+const pkg = require('vitepress/package.json')
+
+export default defineAdditionalConfig({
+  description: 'An organized hub of Islamic online resources.',
+
+  themeConfig: {
+    nav: nav(),
+
+    sidebar: {
+      '/guide/': { base: '/guide/'},
+      '/reference/': { base: '/reference/'}
+    },
+
+    editLink: {
+      pattern: 'https://github.com/HusseinBenz/The-Islamic-Index/',
+      text: 'Edit this page on GitHub'
+    },
+
+  footer: {
+    message: 'Released freely, with no license attached. May it be of benefit to every heart-Muslim and non-Muslim alike 🤍',
+    copyright: 'This project is based on Vitepess. Thanks to Evan You 🌱'
+    }
+
+  }
+})
+
+function nav(): DefaultTheme.NavItem[] {
+  return [
+    {
+      text: 'About Us',
+      link: '/about-us/',
+      activeMatch: '/about-us/'
+    },
+    {
+      text: 'Contact Us',
+      link: '/contact-us/',
+      activeMatch: '/contact-us/'
+    },
+    {
+      text: pkg.version,
+      items: [
+        {
+          text: '1.6.4',
+          link: 'https://vuejs.github.io/vitepress/v1/'
+        },
+        {
+          text: 'Changelog',
+          link: 'https://github.com/vuejs/vitepress/blob/main/CHANGELOG.md'
+        },
+        {
+          text: 'Contributing',
+          link: 'https://github.com/vuejs/vitepress/blob/main/.github/contributing.md'
+        }
+      ]
+    }
+  ]
+}
